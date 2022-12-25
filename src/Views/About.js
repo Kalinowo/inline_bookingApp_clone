00cd559de608f0142800161ec2823a4debc1a5c6
KAL <kevin12147@yahoo.com.tw>
2022-12-25 14:19:12 +0800

    make about page responsive

--- src/Views/About.js
+++ src/Views/About.js
@@ -10,13 +10,13 @@ export default function About() {
             <a
               href="https://github.com/Kalinowo/inline_bookingApp_clone"
               target="_blank"
+              rel="noopener noreferrer"
             >
               https://github.com/Kalinowo/inline_bookingApp_clone
             </a>
           </div>
           <div>
-            此網站為樂排的預約介面克隆，為個人練習及面試使用，如有不妥請聯絡我，我
-            將立即移除。
+            此網站為樂排的預約介面克隆，為個人練習及面試使用，如有不妥請聯絡我，我將立即移除。
           </div>
           <div>
             This site is a clone of inline booking UI for personal practicing
